41a95778c8b30ba4068793faf91ae02fcfcca5e4
Nielsjonathan <nielsjonker64@gmail.com>
2020-10-27 13:22:43 +0100

    cleaned versie

--- Race1.sql
+++
@@ -1,7 +0,0 @@
-SELECT url FROM movies WHERE lexemesSummary @@ to_tsquery('race');
-ALTER TABLE movies
-ADD rank float4;
-UPDATE movies
-SET rank = ts_rank(lexemesSummary,plainto_tsquery((SELECT Summary FROM movies)));
-CREATE TABLE recommendationsBasedOnSummaryField1 AS SELECT url, rank FROM movies WHERE RANK >0.01 ORDER BY rank DESC LIMIT 50;
-\copy (SELECT * FROM recommendationsBasedOnSummaryField1) to '/home/pi/RSL/top50recommendationsRace.csv' WITH csv;
